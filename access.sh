a0ba101beb9c8c2a5ff501627fbad4655b93eeb6
omeshvalyal <108567257+omeshvalyal@users.noreply.github.com>
2023-09-12 23:00:40 +0530

    Update access.sh

--- access.sh
+++ access.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-repo_url="git@github.com:omeshvalyal/new_project.git"
-local_clone_path="/var/lib/jenkins/my_newdir"
+repo_url="git@github.com:omeshvalyal/hello-world-java.git"
+local_clone_path="/var/lib/jenkins/my_newdir2"
 source_branch_name="master"
-git clone --branch "$source_branch_name" --single-branch https://omeshvalyal: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@github.com/omeshvalyal/new_project.git@"$repo_url" "$local_clone_path"
+git clone --branch "$source_branch_name" --single-branch https://omeshvalyal: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@"$repo_url" "$local_clone_path"
