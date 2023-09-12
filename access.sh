d14b76c92766aee8309da224e41ead992361cba1
omeshvalyal <108567257+omeshvalyal@users.noreply.github.com>
2023-09-12 23:32:26 +0530

    Update access.sh

--- access.sh
+++ access.sh
@@ -15,4 +15,4 @@ else
     echo "Folder already exists."
 fi
 
-git clone --branch "$source_branch_name" --single-branch https://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@github.com:omeshvalyal/hello-world-java.git /var/lib/jenkins/my_newdir3
+git clone --branch "$source_branch_name" --single-branch https://omeshvalyal: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@github.com:omeshvalyal/hello-world-java.git /var/lib/jenkins/my_newdir3
