1f5e22f10917a5f522ae6adcbab939dffadaedb8
omeshvalyal <108567257+omeshvalyal@users.noreply.github.com>
2023-09-12 23:08:38 +0530

    Update access.sh

--- access.sh
+++ access.sh
@@ -2,4 +2,17 @@
 repo_url="git@github.com:omeshvalyal/hello-world-java.git"
 local_clone_path="/var/lib/jenkins/my_newdir2"
 source_branch_name="master"
+# Define the folder path and folder name
+path="/var/lib/jenkins"
+folder_name="my_newdir2"
+
+# Check if the folder exists
+if [ ! -d "$path/$folder_name" ]; then
+    echo "Folder does not exist. Creating..."
+    mkdir -p "$path/$folder_name"
+    echo "Folder created successfully."
+else
+    echo "Folder already exists."
+fi
+
 git clone --branch "$source_branch_name" --single-branch "https://omeshvalyal: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@$repo_url" "$local_clone_path"
